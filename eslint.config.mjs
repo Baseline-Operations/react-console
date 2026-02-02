2d89da7fc3fe5e869f3cd1d12a560d0245f7c22f
chrisbrine <chrisbrine.web@gmail.com>
2026-02-02 12:52:39 -0800

    Fixed deprecation warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -64,6 +64,6 @@ export default [
     }
   },
   {
-    ignores: ['dist/**', 'node_modules/**', '**/*.config.js', '**/*.config.mjs', '.lintstagedrc.js']
+    ignores: ['dist/**', 'node_modules/**', '**/*.config.cjs', '**/*.config.mjs', '.lintstagedrc.cjs']
   }
 ];
