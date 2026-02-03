b9cd85f042a7f46ff88da5af5069b31b2db452ae
chrisbrine <chrisbrine.web@gmail.com>
2026-02-02 16:20:33 -0800

    Adjusted workflow

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,34 +1,40 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: '18',
+module.exports = (api) => {
+  // Check if we're building for ESM
+  const isESM = api.env('esm');
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: '18',
+          },
+          modules: isESM ? false : 'commonjs',
         },
-      },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic',
+        },
+      ],
+      [
+        '@babel/preset-typescript',
+        {
+          allowDeclareFields: true,
+        },
+      ],
     ],
-    [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic',
-      },
+    plugins: [
+      // React Compiler - optional but recommended
+      [
+        'babel-plugin-react-compiler',
+        {
+          // Compiler options
+          compilationMode: 'annotation',
+        },
+      ],
     ],
-    [
-      '@babel/preset-typescript',
-      {
-        allowDeclareFields: true,
-      },
-    ],
-  ],
-  plugins: [
-    // React Compiler - optional but recommended
-    [
-      'babel-plugin-react-compiler',
-      {
-        // Compiler options
-        compilationMode: 'annotation',
-      },
-    ],
-  ],
+  };
 };
